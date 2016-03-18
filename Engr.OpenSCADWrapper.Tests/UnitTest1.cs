c2e905f77b3b9130d57a7c781057c305fa8dd9e9
veggielane <chris@lane.org.uk>
2016-03-18 19:58:09 +0000

    First

---
+++ Engr.OpenSCADWrapper.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Engr.OpenSCADWrapper.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+
+        {
+            var openscad = new OpenSCAD();
+            //openscad.Run("");
+            openscad.Generate("");
+        }
+    }
+}
---
+++ Engr.OpenSCADWrapper/OpenSCAD.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engr.OpenSCADWrapper
+{
+    public class OpenSCAD
+    {
+        private string _processFileName;
+        public OpenSCAD(string processFileName = @"C:\Program Files\OpenSCAD\openscad.com")
+        {
+            _processFileName = processFileName;
+        }
+
+        public void Run(string args, bool wait = true)
+        {
+            var process = new Process
+            {
+                StartInfo =
+              {
+                  FileName = _processFileName,
+                  Arguments = args
+              }
+            };
+            process.Start();
+            if (wait)
+            {
+                process.WaitForExit();
+            }
+        }
+
+
+        public Stream Generate(string code, OutputType type = OutputType.STL)
+        {
+            using (var input = new TempFile("cad"))
+            using (var output = new TempFile(type))
+            {
+                return new MemoryStream( File.ReadAllBytes(output.FilePath));
+            }
+        }
+    }
+
+    public enum OutputType { STL, OFF, DXF, CSG }
+    internal class TempFile : IDisposable
+    {
+        public readonly string FilePath;
+
+        public TempFile(OutputType type): this (type.ToString())
+        {
+        }
+
+        public TempFile(string ext)
+        {
+            FilePath = String.Format("{0}{1}.{2}", Path.GetTempPath(), Guid.NewGuid(), ext.ToLower());
+            File.Create(FilePath);
+        }
+
+        public void Dispose()
+        {
+            File.Delete(FilePath);
+        }
+    }
+}
